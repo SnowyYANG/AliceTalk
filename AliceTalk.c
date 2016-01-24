74f5c77a9ff2d461b2c3d9aba9e4282330c789c9
SnowyYANG <SnowyYANG@outlook.com>
2016-01-25 02:00:18 +0800

    clean up

--- AliceTalk.c
+++ AliceTalk.c
@@ -1,8 +1,11 @@
-/*
+﻿/*
 (If you do not understand what is writing in this file, ask someone else for help.)
 
 因为README还会再改所以先删掉README。
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int rand() {
     //return time;
